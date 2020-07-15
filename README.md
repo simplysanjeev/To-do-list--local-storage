dcafdf1b75dbac56b234eb3d22204218ce6c4f80
darkrider2109 <sanjeevkumarv2.0@gmail.com>
2020-07-15 17:24:19 +0530

    add README.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # To-Do-List
+###### A Simple TODO List using HTML, CSS, Vanilla Javascript and localStorage 
+##### Link For TODO LIST
+[TODO List](https://www.google.com)
--- script/script.js
+++ script/script.js
@@ -1,30 +1,98 @@
-let state = {taskList:[], tabStatus:"all"}
-let tempState = localStorage.getItem('state')
-if(tempState != null){
-    state = JSON.parse(tempState); 
+{
+// || Functions in Setting up "state" and store state in Local Storage
+
+//Function to intilize State Variable
+function intializeState(){
+    let tempState = localStorage.getItem('state')
+    if(tempState != null){
+        return JSON.parse(tempState); 
+    }else{
+        return {taskList:[], tabStatus:"all"}
+    }
 }
- 
-// {
-//     taskList : [
-//                  {task: 'task 1', completed: false},
-//                  {task: 'task 2', completed: false},
-//                  {task: 'task 3', completed: true}
-//                ],
-//     tabStatus : "all"   
-//    }
+
+//Function to intialize
 function storeToLocalStorage(){
     localStorage.setItem('state', JSON.stringify(state));
 }
-let message = document.getElementById('message');
-let toDoList = document.getElementById('to-do-list');
-let taskCount = document.getElementById('count');
+
+//Loading "taskList" and "tabStatus" in state Varible
+let state = intializeState();
+
+// || Function to add task to TODO List on click of add task button
 let addTaskButton = document.getElementById('add-task');
 let inputTag = document.getElementById('task-input');
+
+// Function to add task to TODO List
+addTaskButton.addEventListener('click', function(){
+    let taskDescription = inputTag.value.trim(); 
+    inputTag.value = '';
+    if(taskDescription != ''){
+        let item = {task : taskDescription, completed: false};
+        state.taskList.push(item);
+        render(); 
+    }
+});
+
+// Button to :
+// 1. mark all task completed 
+// 2. delete all tasks which are already done
 let markAllTaskCompletedButton = document.querySelector('.complete-all-task');
 let clearCompletedTaskButton = document.querySelector('.clear-completed');
+
+// Function to mark All Task as Completed
+function markAllTaskCompleted(){
+    for(let task of state.taskList){
+        task.completed = true;
+    }
+    render();
+}
+markAllTaskCompletedButton.addEventListener('click', function(){
+    markAllTaskCompleted();
+});
+
+// Function to remove All Completed Task as Completed
+function removeAllCompletedTask(){
+    let tempList = [];
+    for(let item of state.taskList){
+        if(!item.completed){
+            tempList.push(item);
+        }
+    }
+    state.taskList = tempList;
+    render();
+}
+clearCompletedTaskButton.addEventListener('click', function(){
+    removeAllCompletedTask();
+});
+
+//Buttons for Switch Between Tabs of All, Completed, Uncompleted Tasks list
 let switchToAllTaskTabButton =  document.querySelector('#all-tasks');
 let switchToUncompletedTaskTabButton =  document.querySelector('#uncompleted-tasks');
 let switchToCompletedTaskTabButton = document.querySelector('#completed-tasks');
+
+//Function to switch to All Task List Tab
+switchToAllTaskTabButton.addEventListener('click', function(){
+    setTabTextColor();
+    state.tabStatus = "all";
+    render();
+});
+
+//Function to switch to Completed Task List Tab
+switchToCompletedTaskTabButton.addEventListener('click', function(){
+    setTabTextColor();
+    state.tabStatus = "completed";
+    render();
+});
+
+//Function to switch to Uncompleted Task List Tab
+switchToUncompletedTaskTabButton.addEventListener('click', function(){
+    setTabTextColor();
+    state.tabStatus = "uncompleted";
+    render();
+});
+
+// Function to Set Colour Of Tab
 function setTabTextColor(){
     if(state.tabStatus == "all"){
         switchToAllTaskTabButton.style.color = "black";
@@ -40,52 +108,14 @@ function setTabTextColor(){
         switchToCompletedTaskTabButton.style.color = "lightgray";
     }
 }
-switchToAllTaskTabButton.addEventListener('click', function(){
-    setTabTextColor();
-    state.tabStatus = "all";
-    render();
-});
-switchToCompletedTaskTabButton.addEventListener('click', function(){
-    setTabTextColor();
-    state.tabStatus = "completed";
-    render();
-});
-switchToUncompletedTaskTabButton.addEventListener('click', function(){
-    setTabTextColor();
-    state.tabStatus = "uncompleted";
-    render();
-});
-addTaskButton.addEventListener('click', function(){
-    let taskDescription = inputTag.value.trim(); 
-    inputTag.value = '';
-    if(taskDescription != ''){
-        let item = {task : taskDescription, completed: false};
-        state.taskList.push(item);
-        render(); 
-    }
-});
-function markAllTaskCompleted(){
-    for(let task of state.taskList){
-        task.completed = true;
-    }
-    render();
-}
-markAllTaskCompletedButton.addEventListener('click', function(){
-    markAllTaskCompleted();
-});
-function removeAllCompletedTask(){
-    let tempList = [];
-    for(let item of state.taskList){
-        if(!item.completed){
-            tempList.push(item);
-        }
-    }
-    state.taskList = tempList;
-    render();
-}
-clearCompletedTaskButton.addEventListener('click', function(){
-    removeAllCompletedTask();
-});
+
+// || Function to add Event to eventListener to "mark completed task" and "delete a task" button 
+
+let message = document.getElementById('message');
+let toDoList = document.getElementById('to-do-list');
+let taskCount = document.getElementById('count');
+
+
 function addingEventListener(){
     let list = toDoList.childNodes;
     for(let listItem of list){
@@ -107,11 +137,19 @@ function addingEventListener(){
         });
     }
 }
+
+// Function to clear previously rendered List
 function clearList(){
     while(toDoList.hasChildNodes()){
         toDoList.removeChild(toDoList.firstChild);
     }
 }
+
+// Function to count Number of Task Completed By Status 
+// 1. All Task
+// 2. Uncompleted Task
+// 3. Completed Task
+
 function countNumberOfUnCompletedTask(){
     let numberOfTask = 0
     if(state.tabStatus == "all"){
@@ -133,6 +171,8 @@ function countNumberOfUnCompletedTask(){
     }
     return numberOfTask;
 }
+
+// Function to Render List according to status
 function render(){
     clearList();
     setTabTextColor();
@@ -184,3 +224,4 @@ function render(){
 }
 
 render();
+}
